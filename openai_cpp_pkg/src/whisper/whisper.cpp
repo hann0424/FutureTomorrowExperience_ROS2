206acda630cd385df61ccba81abbaedc50236dce
usereim <login3394@gmail.com>
2024-08-19 10:31:32 +0900

    update whisper.cpp

--- openai_cpp_pkg/src/whisper/whisper.cpp
+++ openai_cpp_pkg/src/whisper/whisper.cpp
@@ -3,20 +3,21 @@
 Whisper::Whisper()
     : Node("whisper_node")
 {
-    std::cout << "1" << std::endl;
-    openai_whisper();
+     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+    _question_pub = this->create_publisher<std_msgs::msg::String>("pub_question", qos_profile);
+    _timer = this->create_wall_timer(1s, std::bind(&Whisper::publish_question, this));
 }
 
 void Whisper::publish_question()
 {
-
+    openai_whisper();
+    _question_pub->publish(_question_msg);
 }
 
 void Whisper::openai_whisper()
 {
 
     openai::start();
-    std::cout << "2" << std::endl;
     {
         auto transcription = openai::audio().transcribe(R"(
             {
@@ -24,7 +25,6 @@ void Whisper::openai_whisper()
             "model": "whisper-1"
             }
         )"_json);
-        std::cout << "3" << std::endl;
-        std::cout << "Response is:\n" << transcription.dump(2) << '\n';
+        _question_msg.data = transcription["text"];
     }
 }
