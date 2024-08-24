f39a6362a2ba73b668ae6f670e359323a55ee934
usereim <login3394@gmail.com>
2024-08-24 13:06:31 +0900

    fine-tuning test코드 (성공)

---
+++ fine_tuning/src/fine_tuning.py
@@ -0,0 +1,68 @@
+from openai import OpenAI
+import time
+
+client = OpenAI()
+
+try :
+    response = client.files.create(
+    file=open("../data/mydata.jsonl", "rb"),
+    purpose="fine-tune"
+    )
+
+    file_id = response.id
+
+    print(f"Uploaded File ID: {file_id}")
+    response1 = client.fine_tuning.jobs.create(
+    training_file=file_id,
+    model="gpt-3.5-turbo"
+    )
+
+    fine_tune_id = response1.id
+    client.fine_tuning.jobs.list(limit=10)
+
+
+    # Retrieve the state of a fine-tune
+
+    while True :
+        response2 = client.fine_tuning.jobs.retrieve(fine_tune_id)
+        print(response2.status, "\n")
+
+        if response2.status == "succeeded":
+            fine_tuned_model_id = response2.fine_tuned_model
+            print(fine_tuned_model_id, "\n")
+            break
+
+        elif response2.status == "failed":
+            print("Fine-tuning is not yet complete.")
+            break
+
+        time.sleep(10)
+
+    completion = client.chat.completions.create(
+    model=fine_tuned_model_id,
+    messages=[
+        {"role": "system", "content": "Marv is a chatbot that helps control robot movement."},
+        {"role": "user", "content": "안녕"}
+    ]
+    )
+    print(completion.choices[0].message)
+
+    completion = client.chat.completions.create(
+    model=fine_tuned_model_id,
+    messages=[
+        {"role": "system", "content": "Marv is a chatbot that helps control robot movement."},
+        {"role": "user", "content": "앞으로 가"}
+    ]
+    )
+    print(completion.choices[0].message)
+
+    completion = client.chat.completions.create(
+    model=fine_tuned_model_id,
+    messages=[
+        {"role": "system", "content": "Marv is a chatbot that helps control robot movement."},
+        {"role": "user", "content": "멈춰"}
+    ]
+    )
+    print(completion.choices[0].message)
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
